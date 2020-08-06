86526e41d9deaff0602fc09896cfb38e8ef6d653
Ernest-hub <55037492+Ernest-hub@users.noreply.github.com>
2020-08-06 14:24:15 -0400

    changing the correct quiz_app after replacing

--- book_marks/js/main.js
+++ book_marks/js/main.js
@@ -108,10 +108,13 @@ function fetchBookmarks(){
 
 		bookmarksResults.innerHTML += '<div class="well">'+ 
 									  '<h3>' +name+
-									  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
-									  ' <a  onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+									  '<a class="btn btn-default" target="_blank" href="'+url+'" >Visit</a>'+
+									  '<a  onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a>'+
 									  '</h3>'+
 									  '</div>';
+
+
+									
 
 	}
 
---
+++ quiz_app/question.js
@@ -0,0 +1,72 @@
+var questions = [{
+	"question": "The first mechanical computer designed by Charles Babbage was called ?",
+	"option1": "Abacus",
+	"option2": "Analytical Engine",
+	"option3": "Calculator",
+	"option4": "Processor",
+	"answer": "2"
+}, {
+	"question": "Which of the folowing is the most powerful type of computer ?",
+	"option1": "Super-micro",
+	"option2": "SUper Conductor",
+	"option3": "Super computer",
+	"option4": "Megaframe",
+	"answer": "3"
+}, {
+	"question": "What are programs that tell the computer what to do ?",
+	"option1": "hardware",
+	"option2": "cartridges",
+	"option3": "software",
+	"option4": "printers",
+	"answer": "3"
+}, {
+	"question": "When a meeting is conducted over communication lines on a TV-like device it is called: ?",
+	"option1": "telecommuting",
+	"option2": "electronic mail",
+	"option3": "teleconferencing",
+	"option4": "telemarketing",
+	"answer": "3"
+}, {
+	"question": "The arithmetic/logic unit of the computer: ?",
+	"option1": "can compare numbers",
+	"option2": "can add numbers",
+	"option3": "is part of the central processing unit",
+	"option4": "all of the above",
+	"answer": "4"
+}, {
+	"question": "To work with pictures or charts you use which productivity tool ?",
+	"option1": "Spreadsheet",
+	"option2": "Graphics",
+	"option3": "Database",
+	"option4": "Word processor",
+	"answer": "2"
+}, {
+	"question": "which is a single integrated circuit ?",
+	"option1": "Gate",
+	"option2": "Mother Board",
+	"option3": "Chip",
+	"option4": "CPU",
+	"answer": "1"
+}, {
+	"question": "C is ?",
+	"option1": "A third generation high level language",
+	"option2": "A machine language",
+	"option3": "An assembly language",
+	"option4": "All of the above",
+	"answer": "1"
+}, {
+	"question": "Web pages a written using ?",
+	"option1": "FTP",
+	"option2": "HTTP",
+	"option3": "HTML",
+	"option4": "URL",
+	"answer": "3"
+}, {
+	"question": "UNIVAC is an example of ?",
+	"option1": "First generation computer",
+	"option2": "Second generation computer",
+	"option3": "Third generation computer",
+	"option4": "Fourth generation computer",
+	"answer": "1"
+
+}];
